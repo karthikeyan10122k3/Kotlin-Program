58b0f75ad8074209aa96e9a81d6ecbe0ca75e674
karthikeyan10122k3 <karthikeyan10122k3@gmail.com>
2023-11-29 21:01:48 +0530

    first commit

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+    println("Hello World, This is My First Program in Kotlin!");
+    println("I, Karthikeyan R, have embarked on a journey to acquire proficiency in Kotlin as part of my transition from Java.")
+    println("I have undertaken this endeavor to enhance my skill set and stay abreast of the latest developments in programming languages. The decision to switch from Java to Kotlin aligns with my commitment to continuous learning and adaptability in the dynamic field of software development. I am eager to explore the distinctive features of Kotlin and leverage its advantages for more efficient and modern software development practices.")
+
+}
